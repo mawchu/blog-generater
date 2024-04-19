a0daa771921e18a3dc31f0f6bb437f887c9bf62e
mawchu <mawchu0412@gmail.com>
2024-04-20 00:15:46 +0800

    add logo for post image & update avatar

--- scripts/custom_helpers.js
+++ scripts/custom_helpers.js
@@ -1,11 +1,12 @@
-hexo.extend.tag.register('include_md', function() {
-    const title = this.title.replace(/[^a-zA-Z\s-]/g, '');
-    return `<div class="visual-image">
+hexo.extend.tag.register("include_md", function () {
+  const title = this.title.replace(/[^a-zA-Z\s-]/g, "");
+  return `<div class="visual-image">
         <div class="visual-image-box">
+            <image class="visual-image-deco" src="/blog/images/mawchu_logo_w.svg" />
             <div>${title}</div>
             <div>${title}</div>
             <div>${title}</div>
             <div>．．．</div>
         </div>
-    </div>`
+    </div>`;
 });
